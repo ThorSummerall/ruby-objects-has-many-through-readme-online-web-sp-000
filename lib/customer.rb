525573d350823af82e00b5c5aae80b0fef223046
Thor Summerall <thor.summerall@gmail.com>
2020-12-11 14:34:10 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,8 +1,19 @@
 class Customer
   attr_accessor :name, :age
 
+  @@all = []
+
   def initialize(name, age)
     @name = name
     @age = age
+    save
+  end
+
+  def save
+    self << @@all
+  end
+
+  def self.all
+    @@all
   end
 end
